bdf72442b99ec75812e2fcb1cb1c001798437c78
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2022-05-17 23:11:48 +0530

    LeetCode - Medium 167. Two Sum II - Input Array Is Sorted

---
+++ src/medium/com/leetcode/twosumIIinputarrayissorted_167/Solution.java
@@ -0,0 +1,78 @@
+/*
+
+167. Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+
+*/
+
+package medium.com.leetcode.twosumIIinputarrayissorted_167;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Solution {
+	public int[] twoSum(int[] numbers, int target) {
+		Map<Integer, Integer> numsMap = new HashMap<>();
+		int[] result = new int[2];
+		for(int i=0;i<numbers.length;i++) {
+			int diff = target - numbers[i];
+			if(numsMap.containsKey(diff)) {
+				result[0] = numsMap.get(diff)+1;
+				result[1] = i+1;
+			} else {
+				numsMap.put(numbers[i], i);
+			}
+		}
+		return result;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums = {2,7,11,15};
+		int target = 9;
+		
+//		int[] nums = {2,3,4};
+//		int target = 6;
+//		
+//		int[] nums = {-1,0};
+//		int target = -1;
+		
+		Solution sol = new Solution();
+		System.out.println(Arrays.toString(sol.twoSum(nums,target)));
+	}
+}
