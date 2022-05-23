874e7034ac19b4ab3052c997c670b237390697e8
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2022-05-24 01:38:13 +0530

    LeetCode - Easy 283. Move zeroes

---
+++ src/easy/com/leetcode/movezeroes_283/Solution.java
@@ -0,0 +1,53 @@
+/*
+
+283. Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+
+*/
+
+package easy.com.leetcode.movezeroes_283;
+
+
+public class Solution {
+	public void moveZeroes(int[] nums) {
+		int snowBallSize = 0; 
+        for (int i=0;i<nums.length;i++){
+	        if (nums[i]==0){
+                snowBallSize++; 
+            }
+            else if (snowBallSize > 0) {
+	            int t = nums[i];
+	            nums[i]=0;
+	            nums[i-snowBallSize]=t;
+            }
+        }
+    }
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int nums[] = {0,1,0,3,12};
+		sol.moveZeroes(nums);
+	}
+}
