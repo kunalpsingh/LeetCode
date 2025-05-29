73dd520d0905cdeb30d20d170d4c8b6d74de8657
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-05-29 22:59:18 +0530

    Leet Code Problem 1 Two Sum. https://leetcode.com/problems/two-sum/

---
+++ src/com/kunal/leetcode/Main.java
@@ -0,0 +1,13 @@
+package com.kunal.leetcode;
+
+import com.kunal.leetcode.easy._1_two_sum.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
---
+++ src/com/kunal/leetcode/easy/_1_two_sum/Solution.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/two-sum/
+
+package com.kunal.leetcode.easy._1_two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] sol = new int[2];
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            numsMap.put(nums[i], i);
+        }
+        for(int i=0;i<nums.length;i++) {
+            int complement = target-nums[i];
+            if(numsMap.containsKey(complement) && numsMap.get(complement)!=i) {
+                sol[0] = i;
+                sol[1] = numsMap.get(complement);
+            }
+        }
+        return sol;
+    }
+
+    /*public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }*/
+}
