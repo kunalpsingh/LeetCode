ebcc262935b61d6b804645cbe663036c67046145
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-05-29 23:10:29 +0530

    Leet Code Problem 9 Palindrome Number. https://leetcode.com/problems/palindrome-number/

---
+++ src/com/kunal/leetcode/_9_palindrome_number/Solution.java
@@ -0,0 +1,19 @@
+package com.kunal.leetcode._9_palindrome_number;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        int num = x;
+        if(num<0)
+            return false;
+        int rev = 0;
+        while(num>0) {
+            rev = rev*10 + num%10;
+            num = num/10;
+        }
+        return rev == x;
+    }
+    /*public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(121));
+    }*/
+}
