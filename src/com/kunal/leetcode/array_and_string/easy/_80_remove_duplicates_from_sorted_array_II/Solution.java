0154dbd65d57c0e160d90e2fbe5fd978571a3b79
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-05-31 17:53:25 +0530

    Leet Code Problem 80 Remove Duplicates from Sorted Array II. https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/

---
+++ src/com/kunal/leetcode/array_and_string/easy/_80_remove_duplicates_from_sorted_array_II/Solution.java
@@ -0,0 +1,24 @@
+package com.kunal.leetcode.array_and_string.easy._80_remove_duplicates_from_sorted_array_II;
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length <= 2) return nums.length;
+
+        int i = 2;
+        for (int j = 2; j < nums.length; j++) {
+            // If the current element is not a duplicate of the last two elements
+            if (nums[j] != nums[i - 2]) {
+                nums[i++] = nums[j]; // Place it in the next position
+            }
+        }
+        return i; // Return the new length of the array
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates(new int[]{1,1,1,2,2,3})); // Output: 5
+        System.out.println(solution.removeDuplicates(new int[]{0,0,1,1,1,1,2,3,3})); // Output: 7
+    }
+}
