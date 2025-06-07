64c4e4efb2e870ebb0834cc5f84fe108928dbb5c
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-06-07 14:33:26 +0530

    Leet Code Problem 136 Single Number. https://leetcode.com/problems/single-number/description/

---
+++ src/com/kunal/leetcode/array_and_string/easy/_136_single_number/Solution.java
@@ -0,0 +1,20 @@
+package com.kunal.leetcode.array_and_string.easy._136_single_number;
+
+//https://leetcode.com/problems/single-number/description/
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int num=0;
+        for(int n : nums) {
+            num = num ^ n;
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(new int[]{2,2,1}));
+        System.out.println(solution.singleNumber(new int[]{4,1,2,1,2}));
+        System.out.println(solution.singleNumber(new int[]{1}));
+    }
+}
