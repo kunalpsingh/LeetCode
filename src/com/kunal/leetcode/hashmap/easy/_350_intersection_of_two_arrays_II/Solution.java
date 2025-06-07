b62ca9daa6ec91172fac82acc5b52e39d273adcb
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-06-07 19:31:43 +0530

    Leet Code Problem 350 Intersection of Two Arrays II. Leet Code Problem 66 Intersection of Two Arrays II. https://leetcode.com/problems/plus-one/ Fixed the print statements

--- src/com/kunal/leetcode/hashmap/easy/_350_intersection_of_two_arrays_II/Solution.java
+++ src/com/kunal/leetcode/hashmap/easy/_350_intersection_of_two_arrays_II/Solution.java
@@ -23,8 +23,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Arrays.stream(solution.intersect(new int[]{1,2,2,1}, new int[]{2,2})).forEach(System.out::print);
-        System.out.println(" ");
-        Arrays.stream(solution.intersect(new int[]{4,9,5}, new int[]{9,4,9,8,4})).forEach(System.out::print);
+        System.out.println(Arrays.toString(solution.intersect(new int[]{1,2,2,1}, new int[]{2,2})));
+        System.out.println(Arrays.toString(solution.intersect(new int[]{4,9,5}, new int[]{9,4,9,8,4})));
     }
 }
