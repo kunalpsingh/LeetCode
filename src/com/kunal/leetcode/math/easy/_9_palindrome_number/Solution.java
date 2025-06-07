9022267b08667e0f86501bde6695491383e22410
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-06-07 20:21:11 +0530

    Leet Code Problem 9 Palindrome Number. https://leetcode.com/problems/palindrome-number/ Added leetcode solution post link

--- src/com/kunal/leetcode/math/easy/_9_palindrome_number/Solution.java
+++ src/com/kunal/leetcode/math/easy/_9_palindrome_number/Solution.java
@@ -1,6 +1,7 @@
 package com.kunal.leetcode.math.easy._9_palindrome_number;
 
-//https://leetcode.com/problems/palindrome-number/description/
+//https://leetcode.com/problems/palindrome-number/
+//https://leetcode.com/problems/palindrome-number/solutions/6820334/java-solution-beats-100-ologn-time-o1-sp-sxy4/
 
 public class Solution {
     public boolean isPalindrome(int x) {
