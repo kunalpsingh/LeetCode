f1fc6fd53f04d80c6530d2fd1237bd8c745483be
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-06-07 19:27:06 +0530

    Leet Code Problem 66 Plus One. https://leetcode.com/problems/plus-one/ Fixed the print statements

--- src/com/kunal/leetcode/array_and_string/easy/_66_plus_one/Solution.java
+++ src/com/kunal/leetcode/array_and_string/easy/_66_plus_one/Solution.java
@@ -29,14 +29,10 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        Arrays.stream(solution.plusOne(new int[]{1, 2, 3})).forEach(System.out::print);
-        System.out.println("");
-        Arrays.stream(solution.plusOne(new int[]{4,3,2,1})).forEach(System.out::print);
-        System.out.println("");
-        Arrays.stream(solution.plusOne(new int[]{9})).forEach(System.out::print);
-        System.out.println("");
-        Arrays.stream(solution.plusOne(new int[]{9,9})).forEach(System.out::print);
-        System.out.println("");
-        Arrays.stream(solution.plusOne(new int[]{8,9,9,9})).forEach(System.out::print);
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{4,3,2,1})));
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{9})));
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{9,9})));
+        System.out.println(Arrays.toString(solution.plusOne(new int[]{8,9,9,9})));
     }
 }
