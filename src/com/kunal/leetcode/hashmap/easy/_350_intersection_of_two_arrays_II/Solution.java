20aa76ac0147b1b64e3993c9387d2d84b17d0c17
Kunal Pratap Singh <kunalpsingh1993@gmail.com>
2025-06-07 19:20:55 +0530

    Leet Code Problem 350 Intersection of Two Arrays II. https://leetcode.com/problems/intersection-of-two-arrays-ii/

---
+++ src/com/kunal/leetcode/hashmap/easy/_350_intersection_of_two_arrays_II/Solution.java
@@ -0,0 +1,30 @@
+package com.kunal.leetcode.hashmap.easy._350_intersection_of_two_arrays_II;
+
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/solutions/6820140/java-solution-hash-map-omn-time-omk-spac-7us7/
+
+import java.util.*;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nums1Map = new HashMap<>();
+        List<Integer> solList = new ArrayList<>();
+        for (int num : nums1) {
+            nums1Map.merge(num, 1, Integer::sum);
+        }
+        for(int num : nums2) {
+            if(nums1Map.get(num)!=null && nums1Map.get(num)>0) {
+                solList.add(num);
+                nums1Map.put(num, nums1Map.get(num)-1);
+            }
+        }
+        return solList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Arrays.stream(solution.intersect(new int[]{1,2,2,1}, new int[]{2,2})).forEach(System.out::print);
+        System.out.println(" ");
+        Arrays.stream(solution.intersect(new int[]{4,9,5}, new int[]{9,4,9,8,4})).forEach(System.out::print);
+    }
+}
